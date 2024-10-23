8dbfa9d52f1d5041fe78e741f7efc942c2522c87
DomenikaD <ddelgadoi@est.ups.edu.ec>
2024-10-23 11:59:27 -0500

    first commit

---
+++ src/App.java
@@ -0,0 +1,9 @@
+public class App {
+    public App() {
+    }
+ 
+    public static void main(String[] args) throws Exception {
+       System.out.println("Age sort");
+    }
+ }
+ 
---
+++ src/Ordenamiento.java
@@ -0,0 +1,11 @@
+public class Ordenamiento {
+    public Ordenamiento() {
+        //Ordenamiento: sortByAgeWithInsertion ordenar por edad
+        ordenamienot ();person[]person(String name,int age,int height){
+            return ;void
+            sortByAgeWithInsertion(person[];people):void
+            person(dave, 12, 6,4)
+
+    }
+ }
+ 
